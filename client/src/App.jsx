2fa7fc387704a72c711f5f88f197808f42c26ef9
ravi <ravishekhawatofficial@gmail.com>
2024-08-09 17:50:05 +0530

    add home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { Routes , Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Study Notion
-    </h1>
+    <div>
+      <Routes>
+        <Route path='/'  element={<Home/>} />
+      </Routes>
+
+    </div>
   )
 }
 
